dcb1f363aef936795c5eabe5cc8efe5f5f7aef8c
mariovaquero <mvaquero143@gmail.com>
2024-08-01 21:33:11 -0400

    working on buttons

--- App.py
+++ App.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import Buttons
 
+
+
 class main():
     pygame.init()
     # resolution of window
@@ -16,9 +18,14 @@ class main():
 
     # load images if necessary
     # can be used for UI
+    searchAlgo_Button_Image = pygame.image.load('').convert_alpha()
+    dynamicProg_Button_Image = pygame.image.load('').convert_alpha()
+    sortinAlgo_Button_Image = pygame.image.load('').convert_alpha()
 
     # create new button instances
-    # b_Search = Buttons.button(100,400,image,0.8)
+    searchAlgo_Button = Buttons.button(100,400, searchAlgo_Button_Image ,0.8)
+    dynamicProg_Button = Buttons.button(100,400, dynamicProg_Button_Image, 0.8)
+    sortinAlgo_Button = Buttons.button(100,400, sortinAlgo_Button_Image, 0.8)
 
     # Start loop
     running = True
@@ -31,9 +38,21 @@ class main():
         # Fill in the window so it is not blank
         window.fill((128,128,128))
 
-        # "Draws"  the button on the window
-        # and lets them do what they need to
-
+        # "Draws" the button on the window
+        # and lets them do what they need to do
+        # All buttons will eventually be able to go
+        # to their respecitve section and show
+        #  algorithms or datastructures that are
+        # associated with that section
+        if searchAlgo_Button.draw(window):
+            from Scripts.SearchAlgorithms.BinarySearchScripts.BinarySearchDisplay import binarySearch
+            binarySearch()
+        
+        if dynamicProg_Button.draw(window):
+            print('Dynamic programming button')
+        
+        if sortinAlgo_Button.draw(window):
+            print('Sorting Algorithm button')
         # if b_Search.draw(window):
 
         #updates the entire contents of the display
